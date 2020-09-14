5a0582fbea940e2ad7a33273a088a2758a29d2dc
Venus Chan <vchan446@gmail.com>
2020-09-14 15:37:46 +0800

    added save image

--- javascript/save-image.js
+++ javascript/save-image.js
@@ -1,14 +1,13 @@
+ // Save canvas image as data url 
+ var canvas = document.getElementById("canvas-real");
 
-    // Save canvas image as data url ( default: PNG)
-    var saveButton = document.getElementById('save');
+ var saveButton = document.getElementById('save');
 
-    saveButton.addEventListener('click', function() {
-      var imageName = prompt('Save as...?');
-      var canvasDataURL = canvas.toDataURL();
-      var a = document.createElement('a');
-      a.href = canvasDataURL;
-      a.download = imageName || 'drawing';
-      a.click();
-    });
-  
-  
+ saveButton.addEventListener('click', function () {
+   var imageName = prompt('Save as...?');
+   var canvasDataURL = canvas.toDataURL();
+   var a = document.createElement('a');
+   a.href = canvasDataURL;
+   a.download = imageName || 'drawing';
+   a.click();
+ });
